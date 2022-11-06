24e9535d6b790ad22b3bb1f1059e32c0df2191ee
Huỳnh Đức <hiepthaitran2001@gmail.com>
2022-11-06 14:25:01 +0700

    update

---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿#include "Texture.h"
+#include "Buttons.h"
+#include "Variables.h"
+#include "constant.h"
+#include "function.h"
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+	LFunction game;
+	game.PlayGame();
+	//Free resources and close SDL: giải phóng tài nguyên và đóng SDL
+	game.close();
+	return 0;
+}
+
+
+
